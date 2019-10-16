5192e3db72450e30339e1832c5af51c8dbfa73ff
huongnq <huongnq4@cyberspace.vn>
2019-10-16 16:02:57 +0700

    make it better

--- app.js
+++ app.js
@@ -8,31 +8,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // for parsing application/json
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// support parsing of application/json type post data
-app.use(bodyParser.json());
 
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
